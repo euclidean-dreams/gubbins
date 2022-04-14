e1ddfdb6e825b0f79aa5bff091cd85a80a796e42
Josiah Chapman <josiah.chapman@gmail.com>
2022-04-14 02:06:20 -0700

    first iteration, fits and starts, spazmatic and occasionally smokes. WIll improve

--- src/main.cpp
+++ src/main.cpp
@@ -3,67 +3,122 @@
 
 #include <list>
 
-#define DATA_PIN 15
-#define LED_COUNT 50
-#define BRIGHTNESS 100
-#define COLOR_DRIFT 2
-#define MAX_RAYS 20
-#define RAY_SPAWN_CHANCE 8
+#define DATA_PIN 13
+#define LED_COUNT 300
+#define BRIGHTNESS 20
+#define COMET_SPAWN_RATE 50
+
+int ticksSinceLastComet = 999777;
 
 CRGB leds[LED_COUNT];
 
-struct Ray {
-    CRGB color;
-    int origin;
+struct CometBody
+{
     int size;
+    int locus;
+    CRGB color;
 };
 
-std::list<Ray> rays{};
+struct CometTail
+{
+    CRGB color;
+    float fadeRate;
+};
 
+struct Comet
+{
+    CometBody body;
+    CometTail tail;
+};
 
-CRGB baseColor{static_cast<uint8_t>(random(70)), static_cast<uint8_t>(random(70)), static_cast<uint8_t>(random(70))};
+std::list<Comet> comets{};
 
-void setup() {
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, LED_COUNT);
+Comet createComet(
+
+)
+{
+    CometBody cometBody{
+        random(10),
+        0,
+        {random(100),
+         random(100),
+         random(100)}};
+
+    CometTail cometTail{
+        {random(100),
+         random(100),
+         random(100)},
+        static_cast<float>(random(100)) / 100};
+    comets.push_back({cometBody,
+                      cometTail});
+}
+
+void setup()
+{
+    FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, LED_COUNT);
     FastLED.setBrightness(BRIGHTNESS);
 }
 
-uint8_t driftColor(int color) {
-    int result = color + random(COLOR_DRIFT) - COLOR_DRIFT / 2;
-    while (result < 0) {
-        result += 100;
+// uint8_t driftColor(int color) {
+//     int result = color + random(COLOR_DRIFT) - COLOR_DRIFT / 2;
+//     while (result < 0) {
+//         result += 100;
+//     }
+//     result %= 100;
+//     return result;
+// }
+
+// void boundLED(int index, CRGB color)
+// {
+//     while (index < 0)
+//     {
+//         index += LED_COUNT;
+//     }
+//     index %= LED_COUNT;
+//     leds[index] = color;
+// }
+
+void renderComet(Comet &comet)
+{
+    for (int index = 0; index < comet.body.locus - comet.body.size && index < LED_COUNT; index++)
+    {
+        leds[index] = comet.tail.color;
     }
-    result %= 100;
-    return result;
+    for (int index = comet.body.locus - comet.body.size; index < comet.body.locus + comet.body.size && index < LED_COUNT; index++)
+    {
+        leds[index] = comet.body.color;
+    }
 }
 
-Ray createRay(CRGB baseColor) {
-    return {
-        {driftColor(baseColor.red),
-         driftColor(baseColor.green),
-         driftColor(baseColor.blue)},
-        random(LED_COUNT),
-        1};
-}
-
-void setLed(int index, CRGB color) {
-    while (index < 0) {
-        index += LED_COUNT;
+void loop()
+{
+    if (ticksSinceLastComet % 10 == 0)
+    {
+        createComet();
+        ticksSinceLastComet = 0;
     }
-    index %= LED_COUNT;
-    leds[index] = color;
-}
-
-void loop() {
-    baseColor = CRGB{driftColor(baseColor.red),
-         driftColor(baseColor.green),
-         driftColor(baseColor.blue)};
-    for (int index = 0; index < LED_COUNT; index++) {
-        CRGB varianceColor = CRGB{driftColor(baseColor.red),
-         driftColor(baseColor.green),
-         driftColor(baseColor.blue)};
-        setLed(index, varianceColor);
+    else
+    {
+        ticksSinceLastComet++;
     }
+    for (auto &comet : comets)
+    {
+        renderComet(comet);
+        comet.body.locus++;
+    }
+    // auto iterator = comets.begin();
+    // while (iterator != comets.end()) {
+    //     auto & comet = *iterator;
+    //     renderComet(comet);
+    //     comet.body.locus++;
+    //     iterator++;
+
+    // if (comet.body.locus + comet.body.size > LED_COUNT) {
+    //     iterator = comets.erase(iterator);
+    // } else {
+    //     iterator++;
+    // }
+    // }
     FastLED.show();
-    delay(50);
+    delay(random(50));
 }
